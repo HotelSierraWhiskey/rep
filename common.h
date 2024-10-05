831d02b5fb6ee0c3b69f94f753bc852e22ae3741
HotelSierraWhiskey <winstontothemoon@gmail.com>
2024-10-04 23:21:58 -0400

    Added more ANSI esc seqs

--- common.h
+++ common.h
@@ -24,6 +24,27 @@
 
 #define UNUSED(x)       (void)(x)
 
-#define GREEN(text)     "\033[32m"text"\033[0m"
+/*
+ *  ANSI esc sequences for colours + formatting for stdout
+ */
+#define BOLD(text)              "\033[1m"text"\033[0m"
+#define ITALIC(text)            "\033[3m"text"\033[0m"
+
+#define BLACK(text)             "\033[30m"text"\033[0m"
+#define RED(text)               "\033[31m"text"\033[0m"
+#define GREEN(text)             "\033[32m"text"\033[0m"
+#define YELLOW(text)            "\033[33m"text"\033[0m"
+#define BLUE(text)              "\033[34m"text"\033[0m"
+#define MAGENTA(text)           "\033[35m"text"\033[0m"
+#define CYAN(text)              "\033[36m"text"\033[0m"
+#define WHITE(text)             "\033[37m"text"\033[0m"
+#define BRIGHT_BLACK(text)      "\033[90m"text"\033[0m"
+#define BRIGHT_RED(text)        "\033[91m"text"\033[0m"
+#define BRIGHT_GREEN(text)      "\033[92m"text"\033[0m"
+#define BRIGHT_YELLOW(text)     "\033[93m"text"\033[0m"
+#define BRIGHT_BLUE(text)       "\033[94m"text"\033[0m"
+#define BRIGHT_MAGENTA(text)    "\033[95m"text"\033[0m"
+#define BRIGHT_CYAN(text)       "\033[96m"text"\033[0m"
+#define BRIGHT_WHITE(text)      "\033[97m"text"\033[0m"
 
 #endif

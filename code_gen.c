c0f170fdcee27a053848eecd33c8a4b9cf1c60fb
HotelSierraWhiskey <winstontothemoon@gmail.com>
2024-10-10 19:44:15 -0400

    Cleanup

--- code_gen.c
+++ code_gen.c
@@ -18,6 +18,8 @@
 #define CODE_GEN_ERR(fmt, ...)
 #endif
 
+#define INSTRUCTION_FMT(instruction)	BRIGHT_YELLOW(instruction)
+
 /****************************************************************************************************
  *	T Y P E D E F S
  ****************************************************************************************************/
@@ -40,19 +42,19 @@ typedef struct
 /*
  *	Code generation handlers
  */
-void 							CODE_GEN_handle_EXPR_TYPE_ID				(PARSE_node_t * p_node);
-void 							CODE_GEN_handle_EXPR_TYPE_ADD				(PARSE_node_t * p_node);
-void 							CODE_GEN_handle_EXPR_TYPE_SUBTRACT			(PARSE_node_t * p_node);
-void 							CODE_GEN_handle_EXPR_TYPE_MULTIPLY			(PARSE_node_t * p_node);
-void 							CODE_GEN_handle_EXPR_TYPE_DIVIDE			(PARSE_node_t * p_node);
-void 							CODE_GEN_handle_EXPR_TYPE_ASSIGNMENT		(PARSE_node_t * p_node);
-void 							CODE_GEN_handle_STATEMENT_TYPE_ASSIGNMENT	(PARSE_node_t * p_node);
+static void 					CODE_GEN_handle_node						(PARSE_node_t * p_node);
+static void 					CODE_GEN_handle_EXPR_TYPE_ID				(PARSE_node_t * p_node);
+static void 					CODE_GEN_handle_EXPR_TYPE_ADD				(PARSE_node_t * p_node);
+static void 					CODE_GEN_handle_EXPR_TYPE_SUBTRACT			(PARSE_node_t * p_node);
+static void 					CODE_GEN_handle_EXPR_TYPE_MULTIPLY			(PARSE_node_t * p_node);
+static void 					CODE_GEN_handle_EXPR_TYPE_DIVIDE			(PARSE_node_t * p_node);
+static void 					CODE_GEN_handle_EXPR_TYPE_ASSIGNMENT		(PARSE_node_t * p_node);
+static void 					CODE_GEN_handle_STATEMENT_TYPE_ASSIGNMENT	(PARSE_node_t * p_node);
 
 /*
  *	Helpers
  */
 void 							CODE_GEN_create_label						(void);
-void 							CODE_GEN_emit								(void);
 
 /****************************************************************************************************
  *	F U N C T I O N S
@@ -68,7 +70,6 @@ void CODE_GEN_traverse_tree(PARSE_node_t *p_root)
 	// Handle leaves first
     if (!p_root->p_left && !p_root->p_right)
 	{
-        // printf("visiting leaf %s - %s\n", LEX_get_token_type_descriptor(p_root->p_token->type), p_root->p_token->pc_lexeme);
 		CODE_GEN_handle_node(p_root);
     }
 	else
@@ -83,13 +84,11 @@ void CODE_GEN_traverse_tree(PARSE_node_t *p_root)
 			CODE_GEN_traverse_tree(p_root->p_right);
 		}
 
-		// printf("Visiting non-leaf %s - %s\n", LEX_get_token_type_descriptor(p_root->p_token->type), p_root->p_token->pc_lexeme);
 		CODE_GEN_handle_node(p_root);
 	}
 }
 
-
-void CODE_GEN_handle_node(PARSE_node_t * p_node)
+static void CODE_GEN_handle_node(PARSE_node_t * p_node)
 {
 	if (p_node == NULL)
 	{
@@ -140,16 +139,16 @@ void CODE_GEN_handle_node(PARSE_node_t * p_node)
 	}
 }
 
-void CODE_GEN_handle_EXPR_TYPE_ID(PARSE_node_t * p_node)
+static void CODE_GEN_handle_EXPR_TYPE_ID(PARSE_node_t * p_node)
 {
 	p_node->scratch_register = SCRATCH_REGISTER_alloc();
-	CODE_GEN_DBG("mov %s, %s\n", p_node->p_token->pc_lexeme, 
+	CODE_GEN_DBG(INSTRUCTION_FMT("mov") " %s, %s\n", p_node->p_token->pc_lexeme, 
 		SCRATCH_REGISTER_get_register_name(p_node->scratch_register));
 }
 
-void CODE_GEN_handle_EXPR_TYPE_ADD(PARSE_node_t * p_node)
+static void CODE_GEN_handle_EXPR_TYPE_ADD(PARSE_node_t * p_node)
 {
-	CODE_GEN_DBG("add %s, %s\n", 
+	CODE_GEN_DBG(INSTRUCTION_FMT("add") " %s, %s\n", 
 		SCRATCH_REGISTER_get_register_name(p_node->p_left->scratch_register), 
 		SCRATCH_REGISTER_get_register_name(p_node->p_right->scratch_register));
 
@@ -157,9 +156,9 @@ void CODE_GEN_handle_EXPR_TYPE_ADD(PARSE_node_t * p_node)
 	SCRATCH_REGISTER_free(p_node->p_left->scratch_register);
 }
 
-void CODE_GEN_handle_EXPR_TYPE_SUBTRACT(PARSE_node_t * p_node)
+static void CODE_GEN_handle_EXPR_TYPE_SUBTRACT(PARSE_node_t * p_node)
 {
-    CODE_GEN_DBG("sub %s, %s\n", 
+    CODE_GEN_DBG(INSTRUCTION_FMT("sub") " %s, %s\n", 
         SCRATCH_REGISTER_get_register_name(p_node->p_left->scratch_register), 
         SCRATCH_REGISTER_get_register_name(p_node->p_right->scratch_register));
     
@@ -167,9 +166,9 @@ void CODE_GEN_handle_EXPR_TYPE_SUBTRACT(PARSE_node_t * p_node)
     SCRATCH_REGISTER_free(p_node->p_left->scratch_register);
 }
 
-void CODE_GEN_handle_EXPR_TYPE_MULTIPLY(PARSE_node_t * p_node)
+static void CODE_GEN_handle_EXPR_TYPE_MULTIPLY(PARSE_node_t * p_node)
 {
-    CODE_GEN_DBG("mul %s, %s\n", 
+    CODE_GEN_DBG(INSTRUCTION_FMT("mul") " %s, %s\n", 
         SCRATCH_REGISTER_get_register_name(p_node->p_left->scratch_register), 
         SCRATCH_REGISTER_get_register_name(p_node->p_right->scratch_register));
     
@@ -177,9 +176,9 @@ void CODE_GEN_handle_EXPR_TYPE_MULTIPLY(PARSE_node_t * p_node)
     SCRATCH_REGISTER_free(p_node->p_left->scratch_register);
 }
 
-void CODE_GEN_handle_EXPR_TYPE_DIVIDE(PARSE_node_t * p_node)
+static void CODE_GEN_handle_EXPR_TYPE_DIVIDE(PARSE_node_t * p_node)
 {
-    CODE_GEN_DBG("div %s, %s\n", 
+    CODE_GEN_DBG(INSTRUCTION_FMT("div") " %s, %s\n", 
         SCRATCH_REGISTER_get_register_name(p_node->p_left->scratch_register), 
         SCRATCH_REGISTER_get_register_name(p_node->p_right->scratch_register));
     
@@ -187,12 +186,17 @@ void CODE_GEN_handle_EXPR_TYPE_DIVIDE(PARSE_node_t * p_node)
     SCRATCH_REGISTER_free(p_node->p_left->scratch_register);
 }
 
-void CODE_GEN_handle_EXPR_TYPE_ASSIGNMENT(PARSE_node_t * p_node)
+static void CODE_GEN_handle_EXPR_TYPE_ASSIGNMENT(PARSE_node_t * p_node)
 {
 
 }
 
-void CODE_GEN_handle_STATEMENT_TYPE_ASSIGNMENT(PARSE_node_t * p_node)
+static void CODE_GEN_handle_STATEMENT_TYPE_ASSIGNMENT(PARSE_node_t * p_node)
 {
-
+	p_node->scratch_register = SCRATCH_REGISTER_alloc();
+    CODE_GEN_DBG(INSTRUCTION_FMT("mov") " %s, %s\n", 
+        SCRATCH_REGISTER_get_register_name(p_node->p_right->scratch_register), 
+        SCRATCH_REGISTER_get_register_name(p_node->p_left->scratch_register));
+    
+    SCRATCH_REGISTER_free(p_node->p_right->scratch_register);
 }
--- code_gen.h
+++ code_gen.h
@@ -4,19 +4,6 @@
 #include "common.h"
 #include "parse.h"
 
-typedef enum
-{
-	CODE_GEN_SCRATCH_REGISTER_0 = 0,
-	CODE_GEN_SCRATCH_REGISTER_1,
-	CODE_GEN_SCRATCH_REGISTER_2,
-	CODE_GEN_SCRATCH_REGISTER_3,
-	CODE_GEN_SCRATCH_REGISTER_4,
-	CODE_GEN_SCRATCH_REGISTER_5,
-	//////////////////////////////
-	CODE_GEN_SCRATCH_REGISTER_NUM_SCRATCH_REGISTERS
-} CODE_GEN_scratch_register_id_t;
-
 void CODE_GEN_traverse_tree	(PARSE_node_t * p_root);
-void CODE_GEN_handle_node	(PARSE_node_t * p_node);
 
 #endif
